2c6b8287840de133c7a59b94724b1d0e5a1d5b14
Maaz Khalid <imaazkhalid@gmail.com>
2024-04-05 11:10:10 +0500

    Simple version that converts celsius to fahrenheit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        println!("Enter temperature in Celsius");
+        let mut celsius = String::new();
+
+        io::stdin()
+            .read_line(&mut celsius)
+            .expect("Failed to read line");
+
+        let celsius: f64 = match celsius.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        let fahrenheit: f64;
+
+        fahrenheit = temperature_converter(celsius);
+
+        println!("{fahrenheit}");
+        break;
+    }
+}
+
+fn temperature_converter(celsius: f64) -> f64 {
+    (celsius * 9.0 / 5.0) + 32.0
 }
